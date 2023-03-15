564ad29fd2efd824ae4422d9a0977a14e4122512
Benny Sufrin <bennysuf@gmail.com>
2023-03-14 21:54:59 -0400

    commit tree fc0b29162f2cfff8e40111fa460508f5041a4b53

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,43 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
+      items = Item.all
+    end
+    render json: items, include: :user, status: :ok
+  end
+
+  def show 
+    items = Item.find(params[:id])
+    render json: items, status: :ok
+  end
+
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items.create(
+        name: params[:name], 
+        description: params[:description], 
+        price: params[:price]
+        )
+    else 
+      items = Item.create(
+        name: params[:name], 
+        description: params[:description], 
+        price: params[:price]
+        )
+    end
+    render json: items, status: :created
+  end
+
+  private 
+
+  def render_not_found_response
+    render json: { error: "404 not found" }, status: :not_found
   end
 
 end
